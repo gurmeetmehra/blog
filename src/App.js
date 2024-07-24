225eeb794e3f6ba37718607ab74ab9d310383d94
gurmeet <gurmeetsingh90179@gmail.com>
2024-07-24 21:03:43 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter , Routes, Route,  } from 'react-router-dom';
+import { BrowserRouter , Routes, Route,  } from 'react-router-dom';
 import BlogPostList from './components/BlogPostList';
 import BlogPostDetails from './components/BlogPostDetails';
 import { Toaster } from 'sonner';
@@ -7,12 +7,12 @@ import { Toaster } from 'sonner';
 const App = () => (
   <>
     <Toaster richColors />
-    <HashRouter basename="/blog">
+   
       <Routes>
         <Route path="/" element={<BlogPostList />} />
         <Route path="/post/:id" element={<BlogPostDetails />} />
       </Routes>
-    </HashRouter>
+     
   </>
 );
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BrowserRouter basename="/blog">
     <App />
-
+</BrowserRouter>
   </React.StrictMode>
 );
 
